15a318ea3bbb7ae39c7ba56273535969535d251b
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-11-25 21:06:55 +0500

    Move cursor with arrow keys

--- kilo.c
+++ kilo.c
@@ -12,6 +12,8 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 
+enum editorKey { ARROW_LEFT = 100, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+
 /*** data ***/
 struct editorConfig {
   struct termios orig_term_state;
@@ -70,14 +72,37 @@ void enableRawMode() {
     die("tcsetattr failed");
 }
 
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN)
-      die("read");
+      die("read error");
   }
-  return c;
+  if (c == '\x1b') {
+    char seq[3];
+
+    if (read(STDIN_FILENO, &seq[0], 1) != 1)
+      return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1)
+      return '\x1b';
+
+    // Checking arrow keys
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+      case 'A':
+        return ARROW_UP;
+      case 'B':
+        return ARROW_DOWN;
+      case 'C':
+        return ARROW_RIGHT;
+      case 'D':
+        return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  } else
+    return c;
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -191,24 +216,28 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
   switch (key) {
-  case 'k':
-    E.cy--;
+  case ARROW_UP:
+    if (E.cy != 0)
+      E.cy--;
     break;
-  case 'j':
-    E.cy++;
+  case ARROW_DOWN:
+    if (E.cy != E.screenRows - 1)
+      E.cy++;
     break;
-  case 'h':
-    E.cx--;
+  case ARROW_LEFT:
+    if (E.cx != 0)
+      E.cx--;
     break;
-  case 'l':
-    E.cx++;
+  case ARROW_RIGHT:
+    if (E.cx != E.screenCols - 1)
+      E.cx++;
     break;
   }
 }
 void editorProcessInput() {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
   case CTRL_KEY('q'):
@@ -216,10 +245,10 @@ void editorProcessInput() {
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
-  case 'k':
-  case 'j':
-  case 'h':
-  case 'l':
+  case ARROW_UP:
+  case ARROW_DOWN:
+  case ARROW_LEFT:
+  case ARROW_RIGHT:
     editorMoveCursor(c);
     break;
   }
